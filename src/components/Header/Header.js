21d47ca371af4d6f5d8a37372f29b49f487539f2
Amal Jaison <amaljaison71@gmail.com>
2022-03-09 21:29:51 +0530

    [FEATURE] Header component added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+
+import "./Header.scss";
+import user from "../../images/user.png";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <Link to="/">
+        <div className="logo">Movie App</div>
+      </Link>
+      <div className="user-image">
+        <img src={user} alt="User" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
