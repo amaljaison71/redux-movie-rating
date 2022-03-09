a1136590d14f5faace836b1989a6fa595e234d5b
Amal Jaison <amaljaison71@gmail.com>
2022-03-09 21:00:35 +0530

    [FEATURE] Initial route definitions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import "./App.scss";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="app">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/movie/:imdbId" component={MovieDetail} />
+          <Route component={PageNotFound} />
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
