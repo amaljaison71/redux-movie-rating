4db746df9209b9007c1abc06f8b02c1509333567
Amal Jaison <amaljaison71@gmail.com>
2022-03-10 12:06:00 +0530

    [FEATURE] On clicking movie card load movie detail page

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./MovieCard.scss";
 
 const MovieCard = (props) => {
@@ -5,17 +6,19 @@ const MovieCard = (props) => {
 
   return (
     <div className="card-item">
-      <div className="card-inner">
-        <div className="card-top">
-          <img src={data.Poster} alt={data.Title} />
-        </div>
-        <div className="card-bottom">
-          <div className="card-info">
-            <h4>{data.Title}</h4>
-            <p>{data.Year}</p>
+      <Link to={`/movie/${data.imdbID}`}>
+        <div className="card-inner">
+          <div className="card-top">
+            <img src={data.Poster} alt={data.Title} />
+          </div>
+          <div className="card-bottom">
+            <div className="card-info">
+              <h4>{data.Title}</h4>
+              <p>{data.Year}</p>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
