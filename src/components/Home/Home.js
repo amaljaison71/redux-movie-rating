2141477ec7bc73c57e347571b1037b9bc69f3cee
Amal Jaison <amaljaison71@gmail.com>
2022-03-10 10:22:00 +0530

    [FEATURE] Updating movie store with the fetch movie response

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,9 +4,13 @@ import "./Home.scss";
 import MovieListing from "../MovieListing/MovieListing";
 import movieApi from "../../common/apis/MovieApi";
 import { APIKey } from "../../common/apis/MovieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
 
 const Home = () => {
   const movieText = "Harry";
+  const dispatch = useDispatch();
+
   useEffect(() => {
     const fetchMovies = async () => {
       const response = await movieApi
@@ -15,6 +19,7 @@ const Home = () => {
           console.log("Error: ", err);
         });
       console.log("The response from API: ", response);
+      dispatch(addMovies(response.data));
     };
 
     fetchMovies();
