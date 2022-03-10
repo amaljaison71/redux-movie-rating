14de911a324940a3dab22b925fecb9d03680954e
Amal Jaison <amaljaison71@gmail.com>
2022-03-10 10:01:03 +0530

    [FEATURE] Home componenet and fetching movie list from API

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,30 @@
+import { useEffect } from "react";
+
+import "./Home.scss";
+import MovieListing from "../MovieListing/MovieListing";
+import movieApi from "../../common/apis/MovieApi";
+import { APIKey } from "../../common/apis/MovieApiKey";
+
 const Home = () => {
-  return <div>Home</div>;
+  const movieText = "Harry";
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+        .catch((err) => {
+          console.log("Error: ", err);
+        });
+      console.log("The response from API: ", response);
+    };
+
+    fetchMovies();
+  }, []);
+
+  return (
+    <div className="banner-img">
+      <MovieListing />
+    </div>
+  );
 };
 
 export default Home;
